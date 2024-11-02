81ac574c5aff4ec70e939976c2c897e6f5338222
vinarain <76079506+vinarain@users.noreply.github.com>
2024-11-02 23:34:29 +0700

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
             $(this).addClass("puff-bubble");
         });
         $("#glow").remove();
-        txt.hide().html("i wish you happy birthday").delay(750).fadeIn(300);
+        txt.hide().html("Barakallah Fii Umrik Kak Dhendi").delay(750).fadeIn(300);
         $("#candle").animate(
             {
             opacity: ".5"
